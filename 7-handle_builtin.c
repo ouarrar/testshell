fada3ec3e2f3954027c86731078d2cda26ec5fa4
Abdelouahed Ouarrar <ouarrar.abdelouahed@gmail.com>
2023-09-21 02:33:00 +0100

    v1

--- 7-handle_builtin.c
+++ 7-handle_builtin.c
@@ -8,6 +8,6 @@ void handle_builtin(char **command, char **argv, int *status, int idx)
     if (_strcmp(command[0], "exit") == 0)
         exit_shell(command, status);
 
-    else if (_strcmp(command[8], "env") == 0)
+    else if (_strcmp(command[0], "env") == 0)
         print_env(command, status);
 }
